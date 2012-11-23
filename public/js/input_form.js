6a07cff5c91e152705e5678bb850d65e2262d98a
vikko <vikkosmi@hotmail.com>
2012-11-23 14:16:52 +0100

    css

--- public/js/input_form.js
+++ public/js/input_form.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	$("#_submit_distance_").click(function(e){
 		$.ajax({ 
 			data: {
-				distance: $("#distance_input").val(),
-				heading: $("#heading_input").val()
+				distance: $("#distance_input_field").val(),
+				heading: $("#heading_input_field").val()
 			},
     		type: 'POST', 
     		url: 'Single/update'
